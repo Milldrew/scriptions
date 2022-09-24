e5169bfa0d37d7a0de2db16a58ba3ab4681fbd20
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-09-24 12:30:42 -0400

    📅  | Sat Sep 24 12:30:42 EDT 2022

--- src/scriptions.ts
+++ src/scriptions.ts
@@ -1,4 +1,4 @@
-import { spawnSync, execSync } from "child_process";
+import { execSync } from "child_process";
 const QUESTION_TYPE = "rawlist";
 const SCRIPTS = "scripts";
 const SCRIPTIONS = "scriptions";
