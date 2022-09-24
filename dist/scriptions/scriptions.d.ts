baafaba69edd58002ac59ec926de0aa68ffcc727
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-09-24 09:59:49 -0400

    📅  | Sat Sep 24 09:59:49 EDT 2022

--- dist/scriptions/scriptions.d.ts
+++ dist/scriptions/scriptions.d.ts
@@ -1,1 +1,2 @@
+export {};
 //# sourceMappingURL=scriptions.d.ts.map
--- src/scriptions.ts
+++ src/scriptions.ts
@@ -1,21 +1,13 @@
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
+import inquirer from "inquirer";
+import { QuestionCollection } from "inquirer";
+const QUESTION_COLLECTION: QuestionCollection = [
+  "question 1",
+  "question 2",
+  "question 2",
+  "question 2",
+];
+
+(async () => {
+  console.log("hello iif");
+  const answers = await inquirer.prompt(QUESTION_COLLECTION);
+})();
